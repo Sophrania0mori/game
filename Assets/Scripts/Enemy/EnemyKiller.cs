324325e6c9cd41aa3dcc9361f8e3cb40806bf826
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-03-31 09:32:34 +0200

    Level van Joep geinporteerd
    Package van Joep's level geinporteerd

---
+++ Assets/Scripts/Enemy/EnemyKiller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyKiller : MonoBehaviour
+{
+    [SerializeField] private float raycastDistance = 1f;  // Distance to cast the raycast
+    [SerializeField] private LayerMask enemyLayer;  // Layer mask for the enemy object
+
+    void Update()
+    {
+        // Cast a raycast downwards to check if the player is on top of an enemy
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, enemyLayer);
+
+        // If the raycast hit an enemy, destroy the enemy game object and play the death effect
+        if (hit.collider != null)
+        {
+            hit.collider.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorials/TutorialManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Tilemap ItemTilemap;
+
+    [SerializeField] private GameObject WalkPopUp;
+    [SerializeField] private GameObject JumpPopUp;
+    [SerializeField] private GameObject EnemyPopUp;
+
+    [SerializeField] private GameObject JumpTutorialBox;
+    [SerializeField] private GameObject EnemyTutorialBox;
+    [SerializeField] private GameObject InteractionTutorialBox;
+
+    private bool WalkTutorial = false;
+    private bool JumpTutorial = false;
+    private bool EnemyTutorial = false;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("WalkTutorial")) { WalkTutorial = PlayerPrefs.GetInt("WalkTutorial") == 1 ? true : false; }
+        if (PlayerPrefs.HasKey("JumpTutorial")) { JumpTutorial = PlayerPrefs.GetInt("JumpTutorial") == 1 ? true : false; }
+        if (PlayerPrefs.HasKey("EnemyTutorial")) { EnemyTutorial = PlayerPrefs.GetInt("EnemyTutorial") == 1 ? true : false; }
+
+        if (WalkTutorial)
+        {
+            ItemTilemap.SetTile(new Vector3Int(9, -1, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(9, -2, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(9, -3, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(9, -4, 0), null);
+
+            WalkPopUp.gameObject.SetActive(false);
+            JumpTutorialBox.gameObject.SetActive(true);
+        }
+
+        if (JumpTutorial)
+        {
+            ItemTilemap.SetTile(new Vector3Int(34, -1, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(34, -2, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(34, -3, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(34, -4, 0), null);
+
+            JumpPopUp.gameObject.SetActive(false);
+            EnemyTutorialBox.gameObject.SetActive(true);
+        }
+
+        if (EnemyTutorial)
+        {
+            ItemTilemap.SetTile(new Vector3Int(45, -1, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(45, -2, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(45, -3, 0), null);
+            ItemTilemap.SetTile(new Vector3Int(45, -4, 0), null);
+
+            EnemyPopUp.gameObject.SetActive(false);
+            InteractionTutorialBox.gameObject.SetActive(true);
+        }
+    }
+}
