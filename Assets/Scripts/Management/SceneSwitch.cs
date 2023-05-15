4464bbb1289b504ca44109128a903390d4a689a9
Joep van Dam <joep.vandam@student.hu.nl>
2023-05-15 16:39:02 +0200

    Level 2 toegang mogelijk gemaakt
    Huisje naar beneden en naar links is nu level 2 toegang. Nog geen controle of introlevel gedaan is of niet.

--- Assets/Scripts/Management/SceneSwitch.cs
+++ Assets/Scripts/Management/SceneSwitch.cs
@@ -11,7 +11,12 @@ public class SceneSwitch : MonoBehaviour
     {
         if (PlayerCharacter.position.x > -0.2 && PlayerCharacter.position.x < 0 && PlayerCharacter.position.y > 0.3 && PlayerCharacter.position.y < 0.6)
         {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(2);
+        }
+
+        if (PlayerCharacter.position.x > -2.4 && PlayerCharacter.position.x < -2.2 && PlayerCharacter.position.y > -2.1 && PlayerCharacter.position.y < -1.9)
+        {
+            SceneManager.LoadScene(3);
         }
     }
 }
